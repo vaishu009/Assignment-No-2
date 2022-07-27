f09927a5a8efcc26a5b9b66ca03845a8f66bf488
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 12:23:46 +0530

    Write a program to print numbers from 1 to 10.

---
+++ number1To10.java
@@ -0,0 +1,13 @@
+/*
+Write a program to print numbers from 1 to 10.
+ */
+public class number1To10 
+{
+    public static void main(String srgs[])
+    {
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
