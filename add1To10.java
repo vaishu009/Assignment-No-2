f8b01d5276862def97f5ad270cf438aca5354b7b
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 12:24:45 +0530

    Write a program to add numbers from 1 to 10.

---
+++ add1To10.java
@@ -0,0 +1,15 @@
+/*
+ Write a program to calculate the sum of first 10 natural number.
+ */
+public class add1To10 
+{
+    public static void main(String args[])
+    {
+        int i,sum=0;
+        for(i=1;i<=10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println("sum = "+sum);
+    }
+}
