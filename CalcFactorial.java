8093ecfbe67d2d1085100bc1b780890cf5af8003
vaishu009 <109944617+vaishu009@users.noreply.github.com>
2022-07-27 12:43:29 +0530

    find the factorial value of any number

---
+++ CalcFactorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kanev
+ */
+public class CalcFactorial {
+     public static void main(String[] args) {
+        int num = 6;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
